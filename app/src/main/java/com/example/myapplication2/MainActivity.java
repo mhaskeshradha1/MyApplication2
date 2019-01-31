dae9f1a51198c2ce794703148c45fb12b3590780
Shradha <mhaskeshradha1@gmail.com>
2019-01-31 13:02:20 -0500

    Added toast

--- app/src/main/java/com/example/myapplication2/MainActivity.java
+++ app/src/main/java/com/example/myapplication2/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myapplication2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        Toast.makeText(this, "Montclair State University!",
+                Toast.LENGTH_LONG).show();
+
     }
 }
